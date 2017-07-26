0d96637a5079f0b99a7569d52355a70193a5d7ac
Jonas Luebbers <jluebbers@squarespace.com>
2017-07-26 17:59:15 -0400

    Add empty canvas

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,35 @@
-console.log('Hello, World!')
+const canvas = document.getElementById('intro-canvas')
+const context = canvas.getContext('2d')
+
+const scale = window.devicePixelRatio || 1
+canvas.width = canvas.clientWidth * scale
+canvas.height = canvas.clientHeight * scale
+context.scale(scale, scale)
+
+let animationRequest = null
+
+const entities = []
+
+function start() {
+    if (!animationRequest) {
+        animationRequest = requestAnimationFrame(draw)
+    }
+}
+
+function stop() {
+    if (animationRequest) {
+        cancelAnimationFrame(animationRequest)
+        animationRequest = null
+    }
+}
+
+function draw() {
+    context.clearRect(0, 0, canvas.width, canvas.height)
+
+    context.fillStyle = '#0000ff'
+    context.fillRect(0, 0, canvas.width, canvas.height)
+
+    animationRequest = requestAnimationFrame(draw)
+}
+
+start()
